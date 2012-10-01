ee086abe843dffa44d9d7daad3bbe1eec43da71b
Friso van Vollenhoven <fvanvollenhoven@xebia.com>
2012-10-01 22:16:00 +0200

    Naive log file generator script.

--- data/generate.py
+++ data/generate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import urllib
+import re
 
 movies = map(lambda x: x.strip(), file('movies.txt').readlines())
 
@@ -13,14 +14,17 @@ milli = 0
 
 monthLengths = [-1, 31, 29 if (year % 4 == 0) else 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30]
 
-for month in range(9,10):
-	for day in range(1, 2): #monthLengths[month] + 1):
+pattern = re.compile(r'(.*?)[,(:].*')
+
+for month in range(7,10):
+	for day in range(1, monthLengths[month] + 1):
 		for hour in range(6, 15):
 			for minute in range(0, 60):
 				for second in range(0, 60):
-					term = np.random.zipf(1.3)
+					term = np.random.zipf(1.15)
 					milli = 0.5
 					if term < len(movies):
+						search = pattern.match(movies[term]).groups()[0]
 						print '127.0.0.1\t%4d-%02d-%02dT%02d:%02d:%02d.%03dZ\tGET /search?q=%s HTTP/1.1\t200\t%s' % (
 						year,
 						month,
@@ -28,11 +32,5 @@ for month in range(9,10):
 						hour,
 						minute,
 						second,
-						int(milli * 1000), urllib.quote_plus(movies[term]),
-						movies[term])
-
-
-
-
-							# amount = abs(np.random.normal(6, 1))
-							# for milli in np.random.random(amount):
+						int(milli * 1000), urllib.quote_plus(search),
+						search)
