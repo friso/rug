a1d04c1aae98309c563d9b88ac4a670c6990c1d2
Friso van Vollenhoven <fvanvollenhoven@xebia.com>
2012-10-12 17:23:34 +0200

    Minor updates.

--- nodejs/routes/suggest.js
+++ nodejs/routes/suggest.js
@@ -3,16 +3,21 @@ var client = redis.createClient()
 
 client.on('error', function(err) {
 	//never mind the error, just create a new client
-	client = redis.createClient()
+	console.warn('Could not connect to Redis')
 })
 
 module.exports = function(req, res) {
-	res.set('Content-Type', 'application/json')
-	client.get(req.query['prefix'], function(err, data) {
-		if (data) {
-			res.end(JSON.stringify(data.split('\n')))
-		} else {
-			res.end(JSON.stringify([]))
-		}
-	})
+	if (!client.connected) {
+		res.status(500)
+		res.end('Not connected to Redis')
+	} else {
+		res.set('Content-Type', 'application/json')
+		client.get(req.query['prefix'], function(err, data) {
+			if (data) {
+				res.end(JSON.stringify(data.split('\n')))
+			} else {
+				res.end(JSON.stringify([]))
+			}
+		})
+	}
 }
